5d82d57b3c77d414e34eeed3c98e9c4dff5cbb96
Emmanouil Kalafatis <emmanouil@nichetheory.com>
2026-03-01 10:48:10 +0100

    fixed typo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gr-code-component-main/'
+  base: '/qr-code-component-main/'
 })
